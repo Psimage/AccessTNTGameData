8776ce0c7a87eddd4e8857912a5bbabcaf0b3b0b
Yaroslav.Bugaev <scourgegames@gmail.com>
2017-05-04 00:44:30 +0300

    cleanup

--- AccessTNTGameData.hooks.php
+++ AccessTNTGameData.hooks.php
@@ -11,33 +11,30 @@ class AccessTNTGameDataHooks {
 	public static function onParserFirstCallInit( Parser &$parser ) {
 		global $wgATNTGDGameDataFilename;
 		$game_data_json = file_get_contents( $wgATNTGDGameDataFilename );
+		if ( $game_data_json === false ) {
+			return;
+		}
 		$tnt_data = new TNTGameData( $game_data_json );
 
 		$parser->setFunctionHook( 'tnt_unit_attr', function ( $parser, ...$args ) use ( $tnt_data ) {
-			return self::convertOutput( [ $tnt_data, 'getUnitAttr' ], $args );
+			return self::convertOutput( call_user_func_array( [ $tnt_data, 'getUnitAttr' ], $args ) );
 		} );
 		$parser->setFunctionHook( 'tnt_trait_attr', function ( $parser, ...$args ) use ( $tnt_data ) {
-			return self::convertOutput( [ $tnt_data, 'getTraitAttr' ], $args );
+			return self::convertOutput( call_user_func_array( [ $tnt_data, 'getTraitAttr' ], $args ) );
 		} );
 		$parser->setFunctionHook( 'tnt_wpn_attr', function ( $parser, ...$args ) use ( $tnt_data ) {
-			return self::convertOutput( [ $tnt_data, 'getWeaponAttr' ], $args );
+			return self::convertOutput( call_user_func_array( [ $tnt_data, 'getWeaponAttr' ], $args ) );
 		} );
 		$parser->setFunctionHook( 'tnt_unit_wpn_attr', function ( $parser, ...$args ) use ( $tnt_data ) {
-			return self::convertOutput( [ $tnt_data, 'getUnitWeaponAttr' ], $args );
+			return self::convertOutput( call_user_func_array( [ $tnt_data, 'getUnitWeaponAttr' ], $args ) );
 		} );
 	}
 
-	public static function convertOutput( $callable, $args ) {
-		$res = call_user_func_array( $callable, $args );
-
-		if ( $res === null ) {
-			return "-";
+	public static function convertOutput( $value ) {
+		if ( is_array( $value ) ) {
+			return implode( ',', $value );
 		}
 
-		if ( is_array( $res ) ) {
-			return implode( ',', $res );
-		}
-
-		return $res;
+		return $value;
 	}
 }
